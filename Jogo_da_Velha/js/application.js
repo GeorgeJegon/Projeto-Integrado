d8c629eb3f302efde191e986660225ad81ff9dd5
George Jegon <george_jegon@hotmail.com>
2014-11-04 17:56:03 -0200

    Created NodeTree and tested prototype method to calculate MiniMax Tree

--- Jogo_da_Velha/js/application.js
+++ Jogo_da_Velha/js/application.js
@@ -9,14 +9,55 @@ function isInstanceOf(obj, objClass) {
 var GLOBAL;
 
 window.onload = function () {
-  var g = new Game();
+  var NodeTree = new Node("George"); // Raiz
 
-  GLOBAL = g;
 
-  g.addPlayer(new Player("Humano", "x"));
-  g.addPlayer(new PlayerMaquina("o"));
-  // g.addPlayer(new PlayerMaquina("x"));
-  // g.addPlayer(new Player("Cleiton", "o"));
+  NodeTree.addChild(new Node());
+  NodeTree.addChild(new Node());
+  NodeTree.addChild(new Node());
 
-  g.start();
+
+  NodeTree.get(0).addChild(new Node(3));
+  NodeTree.get(0).addChild(new Node(12));
+  NodeTree.get(0).addChild(new Node(8));
+
+  NodeTree.get(1).addChild(new Node(2));
+  NodeTree.get(1).addChild(new Node(4));
+  NodeTree.get(1).addChild(new Node(6));
+
+  NodeTree.get(2).addChild(new Node(14));
+  NodeTree.get(2).addChild(new Node(5));
+  NodeTree.get(2).addChild(new Node(2));
+
+  //console.log(NodeTree);
+  var test = MiniMaxCalc(NodeTree);
+  console.log(test);
+  console.log(NodeTree);
 };
+
+function MiniMaxCalc (root, level) {
+  level = level || 0;
+
+  var miniMaxValue , select;
+
+  if (!(level%2)) { //Max
+    select = Math.max;
+    miniMaxValue = -Infinity;
+  } else { // Min
+    select = Math.min;
+    miniMaxValue = Infinity;
+  }
+
+  console.log(root.value, "Level: " + level);
+
+  if (!root.isLeaf()) {
+    for (var i = 0, j = root.listNodes.length; i < j; i++) {
+      miniMaxValue = select(miniMaxValue, MiniMaxCalc(root.listNodes[i], level + 1));
+    }
+    root.value = miniMaxValue;
+    console.log(miniMaxValue);
+    return miniMaxValue;
+  } else {
+    return root.value;
+  }
+}
