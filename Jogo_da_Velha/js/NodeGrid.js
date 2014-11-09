fddcc1c3978c3d620ad073ae7d5428ad9c1c9131
George Jegon <george_jegon@hotmail.com>
2014-11-09 14:57:13 -0200

    Added NodeGrid object to project

---
+++ Jogo_da_Velha/js/NodeGrid.js
@@ -0,0 +1,43 @@
+function NodeGrid () {
+  this.grid = new Grid();
+}
+
+NodeGrid.prototype = new Node();
+NodeGrid.prototype.constructor = NodeGrid;
+
+NodeGrid.prototype.setMove = function (field, value) {
+	return this.grid.setMove(field, value);
+};
+
+NodeGrid.prototype.checkWin = function (player) {
+	return this.grid.checkWin(player);
+};
+
+NodeGrid.prototype.clone = function () {
+	var newObject = new NodeGrid(), temp = clone(this.grid);
+
+	newObject.grid.lastMove = temp.lastMove;
+	newObject.grid.matriz = temp.matriz.slice(0);
+	newObject.grid.emptyCells = temp.emptyCells.slice(0);
+
+	return newObject;
+};
+
+NodeGrid.prototype.generateChilds = function (playerSymbol) {
+	var i = this.grid.emptyCells.length, 
+		currentNode, 
+		player = new Player("George" , playerSymbol);
+
+	while (i--) {
+		currentNode = this.clone();
+		currentNode.setMove(currentNode.grid.emptyCells[i], playerSymbol);
+		if (!currentNode.checkWin(player) && !currentNode.grid.checkDraw()) {
+			currentNode.generateChilds((playerSymbol === "o") ? "x" : "o");
+		}
+		this.addChild(currentNode);
+	}
+};
+
+function clone(obj) {
+  return JSON.parse(JSON.stringify(obj));
+}
