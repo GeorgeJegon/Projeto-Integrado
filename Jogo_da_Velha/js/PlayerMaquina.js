0e82c4aa70a8bbb2420cd89f0d0eef18fe39eed5
George Jegon <george_jegon@hotmail.com>
2014-11-16 19:07:12 -0200

    Added method to enableIA lookup tree and some changes

--- Jogo_da_Velha/js/PlayerMaquina.js
+++ Jogo_da_Velha/js/PlayerMaquina.js
@@ -1,12 +1,56 @@
 function PlayerMaquina (symbol) {
   this.setName("Máquina");
   this.setSymbol(symbol);
+  this.useIA = false;
 }
 
 PlayerMaquina.prototype = new Player();
 PlayerMaquina.prototype.constructor = PlayerMaquina;
 
-PlayerMaquina.prototype.makeMove = function (grid) {
+PlayerMaquina.prototype.makeMove = function (grid, gameController) {
+  var newGrid;
+  if (this.useIA) {
+    newGrid = this.chooseBetterMove(grid, gameController);
+  } else {
+    newGrid = this.randomMove(grid);
+  }
+  return newGrid;
+};
+
+PlayerMaquina.prototype.randomMove = function (grid) {
   var field = grid.emptyCells[getRandom(grid.emptyCells.length)];
   return grid.setMove(field, this.symbol);
+};
+
+PlayerMaquina.prototype.enableIA = function () {
+  this.useIA = true;
+  return this;
+};
+
+PlayerMaquina.prototype.disableIA = function () {
+  this.useIA = false;
+  return this;
+};
+
+PlayerMaquina.prototype.chooseBetterMove = function (grid, gameController) {
+  var nodeTreeRoot = gameController.nodeGridTree;
+  if (!nodeTreeRoot.leaf) {
+    var currentNode =  null, maxGrid = nodeTreeRoot.get(0), maxCounter = 0,
+      type = this.getType();
+    for (var i = 1, j = nodeTreeRoot.listNodes.length; i < j; i++) {
+      currentNode = nodeTreeRoot.get(i);
+      // console.log(currentNode.miniMaxValue);
+      // console.table(currentNode.grid.matriz);
+      if ((type === "Max" && currentNode.miniMaxValue > maxGrid.miniMaxValue) ||
+         (type === "Min" && currentNode.miniMaxValue < maxGrid.miniMaxValue)) {
+        maxGrid = currentNode;
+        maxCounter = i;
+      }
+    }
+    console.log(maxGrid.grid.lastMove);
+    gameController.nodeGridTree = nodeTreeRoot.get(maxCounter);
+    return grid.setMove(maxGrid.grid.lastMove, this.getSymbol());
+  } else {
+    console.log("Opa");
+  }
 };
