5c9985350f27080837fee66928601ad9b0ca8693
George Jegon <george_jegon@hotmail.com>
2014-11-17 22:00:34 -0200

    Created a method to debug one level down of nodeGridTree

--- Jogo_da_Velha/js/common.js
+++ Jogo_da_Velha/js/common.js
@@ -13,3 +13,19 @@ function clone(obj) {
 function swapZeroOrOne(value){
 	return (value === 0) ? 1 : 0;
 }
+
+function debugNodeTree (nodeTree) {
+  var root = nodeTree, player = new Player("George", "o");
+
+  if (root && !root.leaf) {
+    for (var i = 0, j = root.listNodes.length; i < j; i++) {
+      console.log(root.get(i).checkWin(player));
+      console.log(root.get(i).getMiniMaxValue());
+      console.table(root.get(i).grid.matriz);
+    }
+  } else {
+    console.log(root.checkWin(player));
+    console.log(root.getMiniMaxValue());
+    console.table(root.grid.matriz);
+  }
+}
